209d9dd02a8ecf2eddd308437a7397f6210c816d
Olafcio1 <75330538+Olafcio1@users.noreply.github.com>
2026-08-23 21:24:01 +0200

    Optimizations & heavy multitasking

--- Modrinth.js
+++ Modrinth.js
@@ -89,8 +89,6 @@
             nocache: true
         })).responseText.replace("\r", "\n").split("\n");
 
-        let oldSL = sloplist;
-
         array = new Set(array);
         sloplist = ``;
 
@@ -101,81 +99,170 @@
             amount++;
         }
 
-        let i = 0, added = 0;
-        for (let slop of array) {
-            let slopids = [];
+        class Executor {
+            #cid;
+            #channels;
+            #maxBuffer;
+            #bufferPolling;
 
+            constructor(channels, { pollingDelay = 10, pollingDiff = 0, maxBuffer = 6, bufferPolling = 200 } = {}) {
+                this.#maxBuffer = maxBuffer;
+                this.#bufferPolling = bufferPolling;
+                this.#channels = new Array(channels);
+                this.#cid = 0;
+
+                for (let zz = 0; zz < channels; zz++) {
+                    const list = [];
+
+                    this.#channels[zz] = list;
+
+                    setTimeout(
+                        () => Executor.#handle(list, pollingDelay),
+                        zz * pollingDiff
+                    );
+                }
+            }
+
+            async execute(fun) {
+                let funs = this.#channels[this.#cid];
+                while (funs.length > this.#maxBuffer)
+                    await new Promise(rsv => setTimeout(rsv, this.#bufferPolling));
+
+                funs.push(fun);
+
+                this.#cid = (this.#cid + 1) % this.#channels.length;
+            }
+
+            async executeTimeouted(fun, timeout) {
+                let funs = this.#channels[this.#cid];
+                while (funs.length > this.#maxBuffer)
+                    await new Promise(rsv => setTimeout(rsv, this.#bufferPolling));
+
+                funs.push(async () => {
+                    await fun();
+                    await new Promise(rsv => setTimeout(rsv, timeout));
+                });
+
+                this.#cid = (this.#cid + 1) % this.#channels.length;
+            }
+
+            static paused = 0;
+
+            static async #handle(list, pollingDelay) {
+                while (true) {
+                    do {
+                        await new Promise(rsv => setTimeout(rsv, pollingDelay));
+                    } while (this.paused > 0);
+
+                    let entries = list.length;
+
+                    for (let fi = 0; fi < entries; fi++) {
+                        await list.shift()();
+                    }
+                }
+            }
+        }
+
+        //TODO Use cors proxies lmfao
+
+        /** request */ let executor = new Executor(3, { pollingDelay: 11, pollingDiff: 2, maxBuffer: 7 });
+        /** storage */ let executor2 = new Executor(3, { pollingDelay: 7, pollingDiff: 1, maxBuffer: 20 });
+        /** block   */ let executor3 = new Executor(1, { pollingDelay: 70, maxBuffer: 20 });
+
+        let getStorage = manual == "reset"  ? id => null  : id => GM_getValue(id);
+
+        function cloudflare() {
+            let iframe = document.createElement("iframe");
+
+            iframe.src = location.href.split("?")[0].split("&")[0].split("slop")[0];
+            iframe.style.position='fixed';
+            iframe.style.inset='0';
+
+            document.body.appendChild(iframe);
+
+            return iframe;
+        }
+
+        async function doRequest(url) {
+            while (true) {
+                try {
+                    let resp = await fetch(url);
+                    if (resp.status == 429) {
+                        Executor.paused++;
+
+                        if (Executor.paused == 1) {
+                            let el = cloudflare();
+                            await new Promise(rsv => setTimeout(rsv, 2400 + ~~(Math.random()*3000)));
+
+                            el.remove();
+                        } else {
+                            await new Promise(rsv => setTimeout(rsv, 2400 + ~~(Math.random()*3000)));
+                        }
+
+                        Executor.paused--;
+
+                        continue;
+                    }
+
+                    return await resp.text();
+                } catch {
+                    await new Promise(rsv => setTimeout(rsv, 1000));
+                }
+            }
+        }
+
+        let i = 0;
+        for (let slop of array) {
             if (slop.startsWith(";") || !slop.trim()) {
                 i++;
                 continue;
             } else if (slop.startsWith("org ")) {
-                let projects;
-                let id = slop.substring(4);
+                const id = slop.substring(4);
 
-                try {
-                    //Force reset
-                    if (manual == "reset")
-                        throw new Error();
-
-                    projects = GM_getValue("o" + id);
-
-                    //TODO Optimize
-                    if (projects)
-                        projects = JSON.parse(projects);
-                    else throw new Error();
-                } catch {
-                    projects ??= await (await fetch(`https://api.modrinth.com/v3/organization/${id}/projects`)).json();
-
-                    (async (projects,id) => {
-                        GM_setValue("o" + id, JSON.stringify(projects));
-                    })(projects,id);
-                }
-
-                if (projects instanceof Array) {
-                    for (let { slug } of projects) {
-                        block(slug);
-                        slopids.push(slug);
+                await executor.executeTimeouted(async () => {
+                    let projects = getStorage("o" + id);
+                    if (!projects) {
+                        projects = await doRequest(`https://api.modrinth.com/v3/organization/${id}/projects`);
                     }
-                }
+
+                    executor2.execute(() => {
+                        GM_setValue("o" + id, projects);
+                    });
+
+                    if (projects[0] == '[') {
+                        let projectsJSON = JSON.parse(projects);
+                        for (const { slug } of projectsJSON) {
+                            executor3.execute(() => block(slug));
+                        }
+                    }
+                }, 40);
             } else if (slop.startsWith("user ")) {
-                let projects;
-                let id = slop.substring(5);
+                const id = slop.substring(5);
 
-                try {
-                    //Force reset
-                    if (manual == "reset")
-                        throw new Error();
-
-                    projects = GM_getValue("u" + id);
-
-                    //TODO Optimize
-                    if (projects)
-                        projects = JSON.parse(projects);
-                    else throw new Error();
-                } catch {
-                    projects ??= await (await fetch(`https://api.modrinth.com/v3/user/${id}/projects`)).json();
-
-                    (async (projects,id) => {
-                        GM_setValue("u" + id, JSON.stringify(projects));
-                    })(projects,id);
-                }
-
-                if (projects instanceof Array) {
-                    for (let { slug } of projects) {
-                        block(slug);
-                        slopids.push(slug);
+                await executor.executeTimeouted(async () => {
+                    let projects = getStorage("u" + id);
+                    if (!projects) {
+                        projects = await doRequest(`https://api.modrinth.com/v3/user/${id}/projects`);
                     }
-                }
+
+                    executor2.execute(() => {
+                        GM_setValue("u" + id, projects);
+                    });
+
+                    if (projects[0] == '[') {
+                        let projectsJSON = JSON.parse(projects);
+                        for (const { slug } of projectsJSON) {
+                            executor3.execute(() => block(slug));
+                        }
+                    }
+                }, 40);
             } else {
-                slopids.push(slop.substring(slop.lastIndexOf("/") + 1));
-                block(slopids[0]);
+                executor3.execute(() => {
+                    block(slop.substring(slop.lastIndexOf("/") + 1));
+                });
             }
 
             container.style.setProperty("--sloplist-full",Math.round((i++/array.size)*100)+"%");
-
-            for (let slopid of slopids)
-                if (!oldSL?.includes?.('"' + slopid + '"'))
-                    added++;
         }
 
         GM_setValue("sloplist", sloplist);
@@ -183,7 +270,7 @@
 
         GM_addStyle(sloplist);
 
-        container.children[0].append((manual ? "Manual" : "First-time") + " slop update performed.\nIndexed slops: " + amount + " (+" + added + ")");
+        container.children[0].append((manual ? "Manual" : "First-time") + " slop update performed.\nIndexed slops: " + amount);
 
         let btn = document.createElement("sloplist-btn");
         btn.innerText = 'Ok';
@@ -202,33 +289,35 @@
 
             PerformUpdate();
         })();
+    } else {
+        GM_addStyle(sloplist);
 
-        return;
-    } else if (!GM_getValue("lastupdate") || Date.now() - parseInt(GM_getValue("lastupdate")) >= DAY) {
-        let el = document.createElement("div");
-        document.body.appendChild(el);
+        if (!GM_getValue("lastupdate") || Date.now() - parseInt(GM_getValue("lastupdate")) >= DAY) {
+            let el = document.createElement("div");
+            document.body.appendChild(el);
+    
+            el.outerHTML = `
+            <div id="update-sloplist-btn" class="btn-wrapper outline text-base" style="display:flex;position:fixed;z-index:3333;left:5px;bottom:20px;scale:.8;outline:none;--_bg:transparent;--_text:var(--color-base);--_hover-bg:transparent;--_hover-text:var(--color-base);--_box-shadow:none;--_height:3rem;--_width:auto;--_radius:1rem;--_padding-x:calc(1rem - 0.125rem);--_padding-y:0.75rem;--_gap:0.5rem;--_font-weight:800;--_icon-size:1.5rem;--_outline-color:var(--surface-5);" data-v-f81a2b1e="" data-v-fd3b428f=""><!--[--><a href="javascript:void(0);" class="" data-v-f81a2b1e="" data-v-fd3b428f-s="">
+            
+            <svg data-v-d971e218="" data-v-fd3b428f-s="" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-1m-4-4-4 4m0 0-4-4m4 4V4"></path></svg>
+            
+            Update SlopList
+            
+            </a><!--]--></div>
+            `;
+    
+            (el = document.querySelector("#update-sloplist-btn")).addEventListener("click", () => {
+                manual = true;
+                PerformUpdate();
+                el.remove();
+            }, { once: true });
+        }
 
-        el.outerHTML = `
-        <div id="update-sloplist-btn" class="btn-wrapper outline text-base" style="display:flex;position:fixed;z-index:3333;left:5px;bottom:20px;scale:.8;outline:none;--_bg:transparent;--_text:var(--color-base);--_hover-bg:transparent;--_hover-text:var(--color-base);--_box-shadow:none;--_height:3rem;--_width:auto;--_radius:1rem;--_padding-x:calc(1rem - 0.125rem);--_padding-y:0.75rem;--_gap:0.5rem;--_font-weight:800;--_icon-size:1.5rem;--_outline-color:var(--surface-5);" data-v-f81a2b1e="" data-v-fd3b428f=""><!--[--><a href="javascript:void(0);" class="" data-v-f81a2b1e="" data-v-fd3b428f-s="">
-        
-        <svg data-v-d971e218="" data-v-fd3b428f-s="" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-1m-4-4-4 4m0 0-4-4m4 4V4"></path></svg>
-        
-        Update SlopList
-        
-        </a><!--]--></div>
-        `;
-
-        (el = document.querySelector("#update-sloplist-btn")).addEventListener("click", () => {
+        GM_registerMenuCommand("Check sloplist updates", () => {
             manual = true;
             PerformUpdate();
-            el.remove();
-        }, { once: true });
+        }, "check-updates");
     }
-
-    GM_registerMenuCommand("Check sloplist updates", () => {
-        manual = true;
-        PerformUpdate();
-    }, "check-updates");
 
     GM_registerMenuCommand("Visit script page", () => {
         GM_openInTab("https://github.com/Olafcio1/SlopList", { active: true });
@@ -237,6 +326,4 @@
     GM_registerMenuCommand("Visit Olafcio1 page", () => {
         GM_openInTab("https://github.com/Olafcio1", { active: true });
     }, "visit-script");
-
-    GM_addStyle(sloplist);
 })();
