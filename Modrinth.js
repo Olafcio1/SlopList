670a7adc1aaea4498f4f5a665e84b1bf51e08af7
Olafcio1 <75330538+Olafcio1@users.noreply.github.com>
2026-08-24 06:48:54 +0200

    Fix fix fix

--- Modrinth.js
+++ Modrinth.js
@@ -234,7 +234,7 @@
                 const id = slop.substring(4);
                 let projects = getStorage("o" + id);
 
-                (async (projects) => await executor[projects ? 'execute' : 'executeTimeouted'](async () => {
+                await (async (projects) => await executor[projects ? 'execute' : 'executeTimeouted'](async () => {
                     if (!projects) {
                         projects = await doRequest(`https://api.modrinth.com/v3/organization/${id}/projects`);
                     }
@@ -254,7 +254,7 @@
                 const id = slop.substring(5);
                 let projects = getStorage("u" + id);
 
-                (async (projects) => await executor[projects ? 'execute' : 'executeTimeouted'](async () => {
+                await (async (projects) => await executor[projects ? 'execute' : 'executeTimeouted'](async () => {
                     if (!projects) {
                         projects = await doRequest(`https://api.modrinth.com/v3/user/${id}/projects`);
                     }
